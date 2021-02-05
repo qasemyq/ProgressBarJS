b7827cdf392cfc3397fbe0b2856f888ee05ba53e
gg <qasemyq@yahoo.com>
2021-02-05 10:41:05 +0100

    bar

---
+++ script.js
@@ -0,0 +1,17 @@
+const bar = document.querySelector('.bar');
+const success = document.querySelector('.success') 
+const counter = document.querySelector('.counter  ') 
+let width = 1; 
+
+function start () {
+    setInterval(function (){
+        if( width >= 100){
+            success.innerHTML = "Download Success"
+            clearInterval();
+        }else {
+            width++;
+            bar.style.width = width + "%"
+            counter.innerHTML = width + "%"
+        }
+    },10);
+}
